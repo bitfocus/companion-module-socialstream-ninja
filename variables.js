03c928a4d3d7d363280b717c5b8a50b445d473b0
bryce <50299051+bryce-seifert@users.noreply.github.com>
2024-07-23 20:49:33 -0500

    v1.0.0
    * initial commit

--- variables.js
+++ variables.js
@@ -1,7 +1,9 @@
-module.exports = async function (self) {
-	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-	])
+export function getVariables() {
+	const variables = []
+
+	variables.push({ variableId: `queue_size`, name: `Queue Size` })
+
+	return variables
 }
+
+export function updateVariables() {}
