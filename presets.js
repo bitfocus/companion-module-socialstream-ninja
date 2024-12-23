8a2b92339221f043bcbecd35848bcf3fe8ced0b9
bryce <50299051+bryce-seifert@users.noreply.github.com>
2024-12-23 13:59:48 -0600

    chore: update to node22, lint (#6)
    * chore: update deps, format
    
    * chore: update to node22
    
    * chore: lint

--- presets.js
+++ presets.js
@@ -1,13 +1,5 @@
-import { combineRgb } from '@companion-module/base'
-
 export function getPresets() {
 	let presets = {}
-
-	const ColorWhite = combineRgb(255, 255, 255)
-	const ColorBlack = combineRgb(0, 0, 0)
-	const ColorRed = combineRgb(200, 0, 0)
-	const ColorGreen = combineRgb(0, 200, 0)
-	const ColorYellow = combineRgb(212, 174, 0)
 
 	return presets
 }
