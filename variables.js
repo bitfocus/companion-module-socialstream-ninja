ed63dd75321de5f5a69dba0878e67e237d786904
bryce <50299051+bryce-seifert@users.noreply.github.com>
2025-01-19 14:41:24 -0600

    feat: feedback/variables for featured message (#9)
    * feat: feedback and variables for current message
    
    * chore: update dependencies
    
    * chore: version up

--- variables.js
+++ variables.js
@@ -1,7 +1,11 @@
 export function getVariables() {
 	const variables = []
 
-	variables.push({ variableId: `queue_size`, name: `Queue Size` })
+	variables.push(
+		{ variableId: `featured_message`, name: `Featured Chat: Message` },
+		{ variableId: `featured_username`, name: `Featured Chat: Username` },
+		{ variableId: `queue_size`, name: `Queue Size` },
+	)
 
 	return variables
 }
